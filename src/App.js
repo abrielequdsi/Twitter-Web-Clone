f2ca9a10a2799c8bd89466f6357acfe1bde733e2
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-08-22 09:41:49 +0700

    add more components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./App.css";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import Widgets from "./Widgets";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
       <Feed />
-      {/* <Widgets /> */}
+      <Widgets />
     </div>
   );
 }
