9c3ffa6d6105504650e809cae72459ae73749e70
Abriele Qudsi <abrielebromozentia@gmail.com>
2020-08-22 09:38:47 +0700

    add styling

--- src/Feed.js
+++ src/Feed.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Feed.css";
 
 function Feed() {
-  return <div></div>;
+  return (
+    <div className="feed">
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 export default Feed;
